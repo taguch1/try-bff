62261e94ef588395d008eba823447144a736a529
taguch1 <taguchi.2240@gmail.com>
2019-04-25 17:16:36 +0900

    add monitoring

---
+++ monitoring/prometheus/.make/helm.mk
@@ -0,0 +1,33 @@
+# Task
+#===============================================================
+helm-cat: .change-cluster
+	helm get $(helm_name)
+
+helm-apply: .change-cluster
+	helm upgrade --install \
+		--namespace $(k8s_namespace) \
+		--values $(helm_dir)/env/$(ENV).yaml \
+		$(helm_apply_options) \
+		$(helm_name) $(helm_dir)
+
+helm-delete: .change-cluster
+	helm delete --purge $(helm_name)
+
+helm-rollback: .change-cluster
+	helm rollback $(helm_name) 0
+
+helm-diff: .change-cluster
+	helm diff \
+		--values $(helm_dir)/env/$(ENV).yaml \
+		$(helm_apply_options) \
+		$(helm_name) $(helm_dir)
+
+helm-restart: .change-cluster
+	$(eval hash := $(shell date +'%Y%m%d%H%M%S'))
+	kubectl set env deployment/$(k8s_deployment) TMP_RECREATE_=$(hash)
+
+# Internal Task
+#===============================================================
+
+.change-cluster:
+	kubectl config set-context $(k8s_context) --namespace=$(k8s_namespace)
