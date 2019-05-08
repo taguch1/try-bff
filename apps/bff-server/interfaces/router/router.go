814b647755c8f164fa5c86f4f9038c4108f347bc
taguch1 <taguchi.2240@gmail.com>
2019-05-08 11:09:59 +0900

    Add api version to url

--- apps/bff-server/interfaces/router/router.go
+++ apps/bff-server/interfaces/router/router.go
@@ -10,6 +10,8 @@ import (
 	"github.com/taguch1/try-bff/apps/bff-server/interfaces/middleware"
 )
 
+const apiVersion = "v1"
+
 // NewHTTPRouter constructor
 func NewHTTPRouter(
 	config *middleware.Config,
@@ -21,20 +23,24 @@ func NewHTTPRouter(
 	r := chi.NewRouter()
 	r.Use(chi_middleware.Recoverer)
 	r.Use(chi_middleware.RequestLogger(log.NewLogFormatter(log.Logger)))
-	r.Use(middleware.NewCors(config.Cors).Handler)
 	r.Use(chi_middleware.RequestID)
 	r.Use(chi_middleware.RealIP)
 	r.Get("/health", healthHandler.Get)
 	r.Get("/metrics", http.HandlerFunc(metricsHandler.ServeHTTP))
-	r.Route("/todos", func(r chi.Router) {
-		r.Post("/", todoHandler.Save)
-		r.Get("/", todoHandler.List)
-		r.Route("/{todoID}", func(r chi.Router) {
-			r.Use(todoHandler.Ctx)
-			r.Get("/", todoHandler.Get)
-			r.Patch("/", todoHandler.Update)
-			r.Delete("/", todoHandler.Delete)
+
+	// version1 apis
+	r.Route(`/v1`, func(r chi.Router) {
+		r.Route("/todos", func(r chi.Router) {
+			r.Post("/", todoHandler.Save)
+			r.Get("/", todoHandler.List)
+			r.Route("/{todoID}", func(r chi.Router) {
+				r.Use(todoHandler.Ctx)
+				r.Get("/", todoHandler.Get)
+				r.Patch("/", todoHandler.Update)
+				r.Delete("/", todoHandler.Delete)
+			})
 		})
 	})
+
 	return r
 }
